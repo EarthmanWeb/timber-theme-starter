fbc22ce1d3c37b1d166ad567f6c2eb66995bca05
Terrance Orletsky <EarthmanWeb@users.noreply.github.com>
2021-12-10 11:14:47 -0800

    Initial commit

---
+++ app/src/theme/functions.php
@@ -0,0 +1,230 @@
+<?php
+
+/**
+ * Contructex Theme
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.1
+ */
+
+
+/* Disable page template cache for dev environment  -  remove for production install */
+function wp_fix_template_caching(WP_Screen $current_screen)
+{
+
+	// Only flush the file cache with each request to post list table, edit post screen, or theme editor.
+	if (!in_array($current_screen->base, array('post', 'edit', 'theme-editor'), true)) {
+		return;
+	}
+
+	$theme = wp_get_theme();
+	if (!$theme) {
+		return;
+	}
+
+	$cache_hash = md5($theme->get_theme_root() . '/' . $theme->get_stylesheet());
+	$label = sanitize_key('files_' . $cache_hash . '-' . $theme->get('Version'));
+	$transient_key = substr($label, 0, 29) . md5($label);
+	delete_transient($transient_key);
+}
+add_action('current_screen', 'wp_fix_template_caching');
+
+/**
+ * Register Custom Navigation Walker to convert WP menus to Bootstrap4
+ */
+function register_navwalker()
+{
+	require_once get_template_directory() . '/vendor/wp-bootstrap/wp-bootstrap-navwalker/class-wp-bootstrap-navwalker.php';
+}
+add_action('after_setup_theme', 'register_navwalker');
+
+/**
+ * Enqueue Bootstrap4 styles
+ */
+function mytimbertheme_styles_hook()
+{
+	$get_bootstrap_css_dir = get_template_directory_uri() . '/vendor/twbs/bootstrap/dist/css/';
+	wp_enqueue_style('bootstrap', $get_bootstrap_css_dir . '/bootstrap.min.css');
+	wp_enqueue_style('core', get_template_directory_uri() . '/style.css');
+}
+add_action('wp_enqueue_scripts', 'mytimbertheme_styles_hook');
+
+/**
+ * Enqueue Bootstrap4 scripts
+ */
+function mytimbertheme_enqueue_scripts()
+{
+	$get_bootstrap_js_dir = get_template_directory_uri() . '/vendor/twbs/bootstrap/dist/js/';
+	wp_enqueue_script('bootstrap', $get_bootstrap_js_dir . '/bootstrap.bundle.min.js', array('jquery'));
+}
+add_action('wp_enqueue_scripts', 'mytimbertheme_enqueue_scripts');
+
+
+/* Include .inc files from includes folder. */
+foreach (glob(__DIR__ . '/includes/*.inc') as $filename) {
+	include_once $filename;
+}
+
+/**
+ * If you are installing Timber as a Composer dependency in your theme, you'll need this block
+ * to load your dependencies and initialize Timber. If you are using Timber via the WordPress.org
+ * plug-in, you can safely delete this block.
+ */
+$composer_autoload = __DIR__ . '/vendor/autoload.php';
+if (file_exists($composer_autoload)) {
+	require_once $composer_autoload;
+	$timber = new Timber\Timber();
+}
+
+/**
+ * This ensures that Timber is loaded and available as a PHP class.
+ * If not, it gives an error message to help direct developers on where to activate
+ */
+if (!class_exists('Timber')) {
+
+	add_action(
+		'admin_notices',
+		function () {
+			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url(admin_url('plugins.php#timber')) . '">' . esc_url(admin_url('plugins.php')) . '</a></p></div>';
+		}
+	);
+
+	add_filter(
+		'template_include',
+		function ($template) {
+			return get_stylesheet_directory() . '/static/no-timber.html';
+		}
+	);
+	return;
+}
+
+/**
+ * Sets the directories (inside your theme) to find .twig files
+ */
+Timber::$dirname = array('templates', 'views');
+
+/**
+ * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
+ * No prob! Just set this value to true
+ */
+Timber::$autoescape = false;
+
+
+/**
+ * We're going to configure our theme inside of a subclass of Timber\Site
+ * You can move this to its own file and include here via php's include("MySite.php")
+ */
+class StarterSite extends Timber\Site
+{
+	/** Add timber support. */
+	public function __construct()
+	{
+		add_action('after_setup_theme', array($this, 'theme_supports'));
+		add_filter('timber/context', array($this, 'add_to_context'));
+		add_filter('timber/twig', array($this, 'add_to_twig'));
+		add_action('init', array($this, 'register_post_types'));
+		add_action('init', array($this, 'register_taxonomies'));
+		parent::__construct();
+	}
+	/** This is where you can register custom post types. */
+	public function register_post_types()
+	{
+	}
+	/** This is where you can register custom taxonomies. */
+	public function register_taxonomies()
+	{
+	}
+
+	/** This is where you add some context
+	 *
+	 * @param string $context context['this'] Being the Twig's {{ this }}.
+	 */
+	public function add_to_context($context)
+	{
+		$context['foo']   = 'bar';
+		$context['stuff'] = 'I am a value set in your functions.php file';
+		$context['notes'] = 'These values are available everytime you call Timber::context();';
+		$context['menu']  = new Timber\Menu();
+		$context['site']  = $this;
+		return $context;
+	}
+
+	public function theme_supports()
+	{
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support('automatic-feed-links');
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support('title-tag');
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support('post-thumbnails');
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support(
+			'html5',
+			array(
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+			)
+		);
+
+		/*
+		 * Enable support for Post Formats.
+		 *
+		 * See: https://codex.wordpress.org/Post_Formats
+		 */
+		add_theme_support(
+			'post-formats',
+			array(
+				'aside',
+				'image',
+				'video',
+				'quote',
+				'link',
+				'gallery',
+				'audio',
+			)
+		);
+
+		add_theme_support('menus');
+	}
+
+	/** This Would return 'foo bar!'.
+	 *
+	 * @param string $text being 'foo', then returned 'foo bar!'.
+	 */
+	public function myfoo($text)
+	{
+		$text .= ' bar!';
+		return $text;
+	}
+
+	/** This is where you can add your own functions to twig.
+	 *
+	 * @param object $twig get extension.
+	 */
+	public function add_to_twig($twig)
+	{
+		$twig->addExtension(new Twig\Extension\StringLoaderExtension());
+		$twig->addFilter(new Twig\TwigFilter('myfoo', array($this, 'myfoo')));
+		return $twig;
+	}
+}
+
+new StarterSite();
